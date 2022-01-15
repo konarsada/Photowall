261509d05120fda3e4eee0130a47d54c85e2a7f6
konarsada <sadabratak@gmail.com>
2022-01-15 20:46:33 +0530

    react router

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class AddPhoto extends Component {
     render() {
-        return <h1>This is the page where we will add Photos</h1>
+        return <div>
+            <h1>Photowall</h1>
+            <div>
+                <form className="form">
+                    <input type="text" placeholder="Link" />
+                    <input type="text" placeholder="Description" />
+                    <button>Post</button>
+                </form>
+            </div>
+        </div>
     }
 }
 
